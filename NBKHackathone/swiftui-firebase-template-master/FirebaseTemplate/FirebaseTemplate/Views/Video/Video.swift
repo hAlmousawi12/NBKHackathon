6c942297f03f9ae5690314670d6810408c6a55aa
HaiderAlboloushi180 <halbolou05@gmail.com>
2021-06-25 18:30:48 +0300

    added my part

---
+++ NBKHackathone/swiftui-firebase-template-master/FirebaseTemplate/FirebaseTemplate/Views/Video/Video.swift
@@ -0,0 +1,21 @@
+//
+//  Video.swift
+//  FirebaseTemplate
+//
+//  Created by Haider Alboloushi on 25/06/2021.
+//  Copyright © 2021 OMAR. All rights reserved.
+//
+
+import SwiftUI
+
+struct Video: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Video_Previews: PreviewProvider {
+    static var previews: some View {
+        Video()
+    }
+}
